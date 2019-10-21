c67e513b975b777d5dd4fe3e9b1b40cb1871c285
FelipeCosta <fac.10@outlook.com>
2019-10-21 13:59:20 -0700

    Added some routes

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,11 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+// internal modules
+const db = require("./models");
+const routes = require("./routes");
+// instanced module
 const app = express();
+
+app.get("/api/v1/sales", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all sales" });
+});
