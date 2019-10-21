4f4a01c26b0a9a6d9afdc36b8c1e88548eb39124
KennyK-TW <b2khakashu@yahoo.com>
2019-10-21 14:01:31 -0700

    merge save test1

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const session = require("express-session");
+// const bodyParser = require("body-parser");
+// const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Routes
-const routes = require("./routes");
+// // Routes
+// const routes = require("./routes");
 
-// ------------------------------------------------- MIDDLEWARE ------------------------------------------------- //
+// // ------------------------------------------------- MIDDLEWARE ------------------------------------------------- //
 
-// BodyParser Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+// // BodyParser Middleware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
-// Serve Public Directory
-app.use(express.static(__dirname + "/public"));
+// // Serve Public Directory
+// app.use(express.static(__dirname + "/public"));
 
 // app.use(session({
 //   secret: 'Sssshhhhhh, this is a secret....',
@@ -25,10 +25,10 @@ app.use(express.static(__dirname + "/public"));
 // ------------------------------------------------- ENDPOINTS ------------------------------------------------- //
 
 // HTML Routes
-app.use("/", routes.views);
+// app.use("/", routes.views);
 
-// API Routes
-app.use("/api/v1", routes.api);
+// // API Routes
+// app.use("/api/v1", routes.api);
 
 // ----------------------------------------------- START SERVER ----------------------------------------------- //
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
