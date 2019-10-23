778e633349a16492143a787bd0ddb6416e158fde
FelipeCosta <fac.10@outlook.com>
2019-10-23 16:56:57 -0700

    wednesday version

--- models/sales.js
+++ models/sales.js
@@ -10,69 +10,65 @@ const saleSchema = new Schema({
     type: String,
     required: true
   },
-  // birthDate: {
-  //   type: Date,
-  //   required: true
-  // },
   email: {
     type: String,
     required: true
+  },
+  birthDate: {
+    type: Date,
+    required: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  country: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  zip: {
+    type: Number,
+    required: true
+  },
+  paymentMethod: {
+    type: String,
+    required: true
+  },
+  nameOnCard: {
+    type: String,
+    required: true
+  },
+  cardNum: {
+    type: String,
+    required: true
+  },
+  expDate: {
+    type: Date,
+    required: true
+  },
+  cvv: {
+    type: Number,
+    required: true
+  },
+  carId: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Car",
+      required: true
+    }
+  ],
+  price: {
+    type: String,
+    required: true
   }
-  // phone: {
-  //   type: String,
-  //   required: true
-  // },
-  // address: {
-  //   type: String,
-  //   required: true
-  // },
-  // country: {
-  //   type: String,
-  //   required: true
-  // },
-  // state: {
-  //   type: String,
-  //   required: true
-  // },
-  // zip: {
-  //   type: Number,
-  //   required: true
-  // },
-  // paymentMethod: {
-  //   type: String,
-  //   required: true
-  // },
-  // nameOnCard: {
-  //   type: String,
-  //   required: true
-  // },
-  // cardNum: {
-  //   type: String,
-  //   required: true
-  // },
-  // expDate: {
-  //   type: Date,
-  //   required: true
-  // },
-  // cvv: {
-  //   type: Number,
-  //   required: true
-  // },
-  // carId: [{
-  //   type: Schema.Types.ObjectId,
-  //   ref: 'Car',
-  //   required: true
-  // }],
-  // // // reference by ID of pokemon
-  // // pokemon: [{
-  // //   type: Schema.Types.ObjectId,
-  // //   ref: 'Pokemon'
-  // // }],
-
-  // price: {
-  //   type: String,
-  //   required: true
-  // }
 });
 
 const newSale = mongoose.model("Sales", saleSchema);
