6a10e7c5ae39e34a50564421aafa82d6492aaef0
KennyK-TW <b2khakashu@yahoo.com>
2019-10-24 12:47:53 -0700

    update button almost there

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,10 +1,9 @@
 onSuccess = response => {
   console.log("success!");
 
-  response.data.forEach((newSale, cars) => {
+  response.data.forEach((newSale) => {
     const template = `
-      <tr>
-      <button>Delete</button>
+      <tr id=${newSale._id}>
       <td>${newSale.firstName}</td>
       <td>${newSale.lastName}</td>
       <td>${newSale.email}</td>
@@ -20,11 +19,15 @@ onSuccess = response => {
       <td>${newSale.cvv}</td>
       <td>${newSale.carId}</td>
       <td>${newSale.price}</td>
+      <td><input type="checkbox" id="checkbox" name="checkbox" value="true"></td>
+      <td><button class="update">Update</button></td>
+      <td><button class="delete">Delete</button></td>
       </tr>
       `;
     $("tbody").append(template);
   });
 };
+
 
 onError = () => {
   console.log("err");
@@ -40,3 +43,59 @@ $.ajax({
 $("button").click(function() {
   event.preventDefault();
 });
+
+$('#table-body').on('click', '.update', (event) => {
+  // console.log(event.target.parentNode.parentNode);
+  // const id = event.target.parentNode;
+  $(event.target.parentNode).replaceWith(`<p>Replaced</p>`)
+  
+  // const id = event.target.parentNode.parentNode.id;
+  // const data = event.target.parentNode.parentNode.data;
+  // console.log(id);
+  // updateProfile(id, data);
+  // $(event.target.parentNode.parentNode).replaceWith(a);
+
+});
+
+$('#table-body').on('click', '.delete', (event) => {
+  console.log(event.target.parentNode.parentNode);
+  // console.log(event.currentTarget.parentElement.parentElement);
+  const id = event.target.parentNode.parentNode.id;
+  console.log(id);
+  deleteProfile(id);
+  $(event.target.parentNode.parentNode).empty();
+  
+  // console.log(`${sales.data._id}`);
+});
+
+const updateProfile = (id, data) => {
+  $.ajax({
+    method: "PUT",
+    url: `/api/v1/sales/${id}`,
+    data: data,
+    success: (res)=>{console.log(res)},
+    error: (err)=>{console.log(err)},
+  })
+}
+
+
+const deleteProfile = (id) => {
+  $.ajax({
+    method: "DELETE",
+    url: `/api/v1/sales/${id}`,
+    success: (res)=>{console.log(res)},
+    error: (err)=>{console.log(err)},
+  })
+}
+
+
+
+// $(".squares").on("click",".square", (event) => {
+//   handlePoke(event);
+// });
+
+// const confirmPaid =  event => {
+//   if(event.target.classList.contains("poked") === false){
+//     $(event.target).addClass("poked").css("opacity",.3);
+//   }
+// };
